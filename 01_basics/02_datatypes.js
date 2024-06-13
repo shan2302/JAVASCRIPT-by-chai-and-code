369abaf883b03ffbd32d1af201f032a544e580bc
shan2302 <shantanu230205@gmail.com>
2024-06-13 16:43:04 +0530

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,6 +1,21 @@
 //WE SHOULD DOWNLOAD PRETTIER,GITHUB PULL REQUEST,GITHUB REPOSITORIES,JS AND TS,LIVE SERVER,AZURE REPOS,NODEJS,STANDARD JS,REMOTE REPOSITORIES
+//tc39.es is important website for standard of js
 "use strict"; //use all js code as newer version
 //alert("hello") //we are using node js,not browser
 console.log(3+3); console.log("Shantanu")
 console.log(3+3);
 console.log("SHANTANU"); //CODE READABILITY SHOULD BE HIGH
+
+let name = "shantanu"
+let age = 19
+let islogged = false
+let state;
+//number => 2 to the power 53
+//bigint
+//string => "" or ''
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol =>  unique
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
