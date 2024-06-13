1fc66260d982884ab089efe2ebbac87059b12410
shan2302 <shantanu230205@gmail.com>
2024-06-13 14:33:58 +0530

    COMMIT

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,6 @@
+//WE SHOULD DOWNLOAD PRETTIER,GITHUB PULL REQUEST,GITHUB REPOSITORIES,JS AND TS,LIVE SERVER,AZURE REPOS,NODEJS,STANDARD JS,REMOTE REPOSITORIES
 "use strict"; //use all js code as newer version
-//alert("hello") //we are using node js
+//alert("hello") //we are using node js,not browser
 console.log(3+3); console.log("Shantanu")
+console.log(3+3);
+console.log("SHANTANU"); //CODE READABILITY SHOULD BE HIGH
