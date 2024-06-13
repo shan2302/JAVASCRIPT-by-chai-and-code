0210b3138ce257c8fab5ab09d54c9d04bc316ad7
shan2302 <shantanu230205@gmail.com>
2024-06-13 17:19:14 +0530

    conversion_of_datatypes

---
+++ 01_basics/03_conversion_operations.js
@@ -0,0 +1,22 @@
+let score = "34abc"
+console.log(typeof score);
+let valueinNumber = Number(score)
+console.log(typeof valueinNumber);
+console.log( valueinNumber);
+
+// "33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isloggedIn = "Singh"
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//""=> false
+// "singh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
