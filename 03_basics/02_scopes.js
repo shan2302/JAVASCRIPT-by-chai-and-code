7b115ed30b55fac2cad90445ee44dacfa0f47843
Shan2302 <shantanu230205@gmail.com>
2024-07-02 21:27:08 +0530

    scope_level_and_declartion_of_function

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,3 +9,39 @@ if (true) {
  console.log(a);
 //  console.log(b);
 // console.log(c);
+
+function one(){
+    const userName = "shantanu singh"
+
+    function two(){
+        const website= "youtube"
+        console.log(userName);
+    }
+    // console.log(website);
+
+    two()
+}
+// one()
+
+if(true){
+    const userName = "Shantanu"
+    if(userName === "Shantanu"){
+        const website = " Youtube"
+        console.log(userName+website);
+    }
+    // console.log(website);
+}
+// console.log(userName);
+
+
+// +++++++++++++++++++++++++++++++INTERESTING+++++++++++++++++++++++++
+addOne(5)
+function addOne(num){
+    return num+1
+}
+
+// addTwo(5) this is not allowed because we call function before declare as a const function
+const addTwo = function(num){
+    return num+2
+}
+addTwo(5)
