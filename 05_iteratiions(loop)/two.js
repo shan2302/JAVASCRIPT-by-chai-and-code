27120c49c2759e7f994bbd50e6bd00a1c05c53af
Shan2302 <shantanu230205@gmail.com>
2024-07-12 17:22:03 +0530

    while_dowhile

---
+++ 05_iteratiions(loop)/two.js
@@ -0,0 +1,18 @@
+let i = 0
+while (i<=10) {
+    console.log(`Value of i is ${i}`);
+    i=i+2
+}
+let MyArray=["flash", "batman", "superman"]
+
+let arr=0
+while (arr< MyArray.length) {
+    console.log(`Value is ${MyArray[arr]}`);
+    arr= arr+1
+}
+
+let score=1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score<=10);
